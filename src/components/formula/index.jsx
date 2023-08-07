20ea3a362c088f6b501444b08c3080cc81f6e66f
Illia Kozyr <ilunya.kozyr@gmail.com>
2023-08-07 21:10:05 +0300

    tax done

--- src/components/formula/index.jsx
+++ src/components/formula/index.jsx
@@ -8,27 +8,26 @@ export const CompoundInterestFunc = (
     additionalContribution,
     tax
 ) => {
+    // Перетворимо процентну ставку у десятковий формат
+    const rateDecimal = rate / 100;
+    // Розраховуємо загальну кількість періодів
+    const periods = n * time;
+
+    let amountMinusTax;
+    let taxPaid;
+    let amount = principal;
+
     // principal - початкова сума (початковий капітал)
     // rate - річна процентна ставка у відсотках
     // time - кількість років (термін вкладу)
     // n - частота складання відсотків за рік (річна, піврічна, квартальна, місячна тощо)
     // additionalContribution - додатковий внесок на кожен період
 
-    // Перетворимо процентну ставку у десятковий формат
-    const rateDecimal = rate / 100;
-
-    // Розраховуємо загальну кількість періодів
-    const periods = n * time;
-
     // Розраховуємо складний відсоток з додатковими внесками
-    let amount = principal;
     for (let i = 0; i < periods; i++) {
         amount = amount * (1 + rateDecimal / n) + additionalContribution;
         store.addNumberForOnePeriod(parseFloat(amount.toFixed(2)));
     }
-
-    let amountMinusTax;
-    let taxPaid;
 
     amountMinusTax = amount - amount * 0.195;
     store.addTax(parseFloat(amountMinusTax.toFixed(2)));
